0e68504ea4471885d0d0b7f055734967cc210c99
Kristina <36459922+Kristute@users.noreply.github.com>
2018-04-10 23:26:01 +0300

    Add files via upload

---
+++ registracija.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-ca">
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <title>The Site</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<header>
+    <div class="back">
+    <h1> Your message has been successfully sent! <h1>
+        <a href="index.html"> << Back to page</a>
+    </div>
+</header>
+</body>
+</html>
